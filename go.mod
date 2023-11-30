e7ea3e18892d5e968f83be2e3ac80e3db4abbf9f
Jayden Sleeuwenhoek <sleeuwenhj@coventry.ac.uk>
2023-11-30 13:07:29 +0000

    initialized project and created main file which takes user input to get a network interface, loops through the packets caught and prints to screen

---
+++ go.mod
@@ -0,0 +1,9 @@
+module packet_analyzer
+
+go 1.21.4
+
+require (
+	github.com/google/gopacket v1.1.19 // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	golang.org/x/tools v0.16.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	// Choose the network device to capture on
+	fmt.Println("Enter The Network Device to capture on (by MAC Address): ")
+	// Taking input from user
+	var selectedDevice string
+	fmt.Scanln(&selectedDevice)
+
+	// Open the selected network device for packet capture
+	handle, err := pcap.OpenLive(selectedDevice, 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal("Error opening adapter:", err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	// Capture packets until stopped
+	for packet := range packetSource.Packets() {
+		// Process the captured packet
+		fmt.Println(packet)
+	}
+}
